0a7d70a7d57220ce66cfbe06954a258c03798554
Huawei User <gariban_bir_kul@gariban_bir_kul.com>
2023-08-06 16:38:31 +0300

    It has been modified

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import turtle
 import time
 import random
-
+SCORE_Y = turtle.window_height() * 0.45
+TIME_Y = turtle.window_height() * 0.4
 counter = 0
 
 turtle_screen = turtle.Screen()
@@ -16,8 +17,6 @@ def score_update(x, y):
     global counter
     instance3.clear()
     instance3.setx(0)
-    print(f"x coord : {x}\ny coord : {y}")
-    print("ss::", turtle_instance.xcor(), "--", turtle_instance.ycor())
     tur_x = turtle_instance.xcor()
     tur_y = turtle_instance.ycor()
     click_state = (x < tur_x - 7 or x > tur_x + 7) or (y < tur_y - 7 or y > tur_y + 15)
@@ -34,45 +33,54 @@ instance3 = turtle.Turtle()
 instance3.color("black")
 instance3.hideturtle()
 instance3.penup()
-instance3.goto(0,300)
+instance3.goto(0, SCORE_Y)
 instance3.write("Score", move=True, font=text_font)
 
-instance2 = turtle.Turtle()
-instance2.color("black")
-instance2.hideturtle()
-instance2.penup()
-instance2.goto(0, 260)
-instance2.write("Time", move=True, font=text_font)
+timer_instance = turtle.Turtle()
+timer_instance.color("black")
+timer_instance.hideturtle()
+timer_instance.penup()
+timer_instance.goto(0, TIME_Y)
+timer_instance.write("Time", move=True, font=text_font)
 
 
 text = "Game Over"
-second: int = 20
+second: int = 5
 
 turtle_screen.onclick(score_update)
 
 
 def turtle_random_move():
+    global second
     turtle_x = random.randint(-200, 200)
     turtle_y = random.randint(-200, 200)
     turtle_instance.hideturtle()
     turtle_instance.goto(turtle_x, turtle_y)
     turtle_instance.showturtle()
+    if(second >= 0):
+        turtle_screen.ontimer(turtle_random_move, 2000)
+    else:
+        turtle_instance.hideturtle()
 
 
+turtle_random_move()
 
-while second >= 0:
-    instance2.clear()
-    instance2.setx(0)
-    instance2.write(f"Time : {second}", font=text_font)
-    time.sleep(1)
+
+def countdown_timer():
+    global second
+    timer_instance.clear()
+    timer_instance.setx(0)
+    timer_instance.write(f"Time : {second}", font=text_font)
     second -= 1
-    turtle_random_move()
+    if(second >= 0):
+        turtle_screen.ontimer(countdown_timer, 1000)
+    else:
+        timer_instance.clear()
+        timer_instance.setx(0)
+        timer_instance.write(f"{text}", font=text_font)
 
-instance2.clear()
-instance2.setx(0)
-instance2.write(f"{text}", font=text_font)
 
-turtle_instance.hideturtle()
+countdown_timer()
 
 
 turtle_screen.mainloop()
